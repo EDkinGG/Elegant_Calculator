da3111e69475cf4a1dea7cb7664c62a49b78ac48
EDkinGG <r23ghani@gmail.com>
2022-05-09 23:49:15 +0600

    Modified Br1 from master

--- app/src/androidTest/java/Branch_test/TestingBranch.java
+++ app/src/androidTest/java/Branch_test/TestingBranch.java
@@ -4,16 +4,25 @@ public class TestingBranch {
     String name;
     int id;
     float marks;
+    int streetNo;
 
-    TestingBranch( String n , int i , float m )
-    {
-        this.name = n;
-        this.id = i;
-        this.marks = m;
+    public TestingBranch(String name, int id, float marks, int streetNo) {
+        this.name = name;
+        this.id = id;
+        this.marks = marks;
+        this.streetNo = streetNo;
+    }
+
+    public int getStreetNo() {
+        return streetNo;
     }
 
     public int getId() {
         return id;
+    }
+
+    public void setStreetNo(int streetNo) {
+        this.streetNo = streetNo;
     }
 
     public String getName() {
